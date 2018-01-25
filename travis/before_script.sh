81254f3007e56b6f0f3de81345c50d576f65fa67
Kerry Billingham <contact@AvionicEngineers.com>
2018-01-25 22:28:49 +0000

    gpg silent mode.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -7,5 +7,5 @@ if [ "$TRAVIS_BRANCH" = 'dev' ]; then
     openssl aes-256-cbc -K $encrypted_4f3061b44f4c_key -iv $encrypted_4f3061b44f4c_iv -in ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml.enc -out ${TRAVIS_BUILD_DIR}/travis/encrypt-settings.xml -d
 
     openssl aes-256-cbc -K $encrypted_6546a769d586_key -iv $encrypted_6546a769d586_iv -in ${TRAVIS_BUILD_DIR}/travis/codesigning.asc.enc -out ${TRAVIS_BUILD_DIR}/travis/codesigning.asc -d
-    gpg --fast-import ${TRAVIS_BUILD_DIR}/travis/codesigning.asc
+    gpg --fast-import --no-tty ${TRAVIS_BUILD_DIR}/travis/codesigning.asc &> /dev/null
 fi
