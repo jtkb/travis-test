dfd4005d4ff40ebce59c96be027ab7cdde76382a
Kerry Billingham <contact@AvionicEngineers.com>
2018-01-27 09:51:28 +0000

    Testing version checking on merge to master.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
-# if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+
+echo "TRAVIS_BRANCH = ${TRAVIS_BRANCH}"
+echo "TRAVIS_PULL_REQUEST = ${TRAVIS_PULL_REQUEST}"
+
+# Test if merge to master that it is not still in SNAPSHOT
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+    PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -v ^\\[INFO\\])
+    echo "Project Version: ${PROJECT_VERSION}"
+    echo ${PROJECT_VERSION} | grep \\-SNAPSHOT
+    if [ $? -eq 0 ]; then
+        # version ends with -SNAPSHOT
+        echo "The version ends with -SNAPSHOT."
+        exit 1
+    fi
+fi
+
 if [ "$TRAVIS_BRANCH" = 'dev' ]; then
     mkdir ~/.m2
     cp ${TRAVIS_BUILD_DIR}/travis/settings-security.xml ~/.m2/
@@ -9,3 +24,6 @@ if [ "$TRAVIS_BRANCH" = 'dev' ]; then
     openssl aes-256-cbc -K $encrypted_6546a769d586_key -iv $encrypted_6546a769d586_iv -in ${TRAVIS_BUILD_DIR}/travis/codesigning.asc.enc -out ${TRAVIS_BUILD_DIR}/travis/codesigning.asc -d
     gpg --fast-import --no-tty ${TRAVIS_BUILD_DIR}/travis/codesigning.asc &> /dev/null
 fi
+
+# TODO: Remove to enable build to proceed.
+exit 1
